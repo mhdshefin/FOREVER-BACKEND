5ba6132ced329db67732378cee098bbff178db9f
mhdshefin <rishnabinthsalam@gmail.com>
2025-01-18 22:58:43 +0530

    first commit

--- Middleware/adminAuth.js
+++ Middleware/adminAuth.js
@@ -3,16 +3,16 @@ import jwt from 'jsonwebtoken'
 
 const adminAuth = async (req, res, next) => {
 
-    try {
+    try { 
 
         const { token } = req.headers;
 
         if (!token) {
-            return res.json({ success: false, message: "Not Authorized login again" })
+            return res.json({ success: false, message: "Not Authorized login again." })
         }
         const token_decoded = jwt.verify(token, process.env.JWT_SECRET)
         if (token_decoded !== process.env.ADMIN_EMAIL + process.env.ADMIN_PASSWORD) {
-            return res.json({ success: false, message: "Not Authorized login again" })
+            return res.json({ success: false, message: "Not Authorized login again." })
         }
         next()
     } catch (error) {
