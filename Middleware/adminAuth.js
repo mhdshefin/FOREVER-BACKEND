f7dd2c5667a8c4f32a40924d7a7ee119b6ab1d84
mhdshefin <rishnabinthsalam@gmail.com>
2025-01-18 23:04:59 +0530

    first commit

--- Middleware/adminAuth.js
+++ Middleware/adminAuth.js
@@ -8,11 +8,11 @@ const adminAuth = async (req, res, next) => {
         const { token } = req.headers;
 
         if (!token) {
-            return res.json({ success: false, message: "Not Authorized login again." })
+            return res.json({ success: false, message: "Not Authorized login again" })
         }
         const token_decoded = jwt.verify(token, process.env.JWT_SECRET)
         if (token_decoded !== process.env.ADMIN_EMAIL + process.env.ADMIN_PASSWORD) {
-            return res.json({ success: false, message: "Not Authorized login again." })
+            return res.json({ success: false, message: "Not Authorized login again" })
         }
         next()
     } catch (error) {
