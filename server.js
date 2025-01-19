8adc52d67acc0197b625e5fad39514ea0cf4fbba
mhdshefin <rishnabinthsalam@gmail.com>
2025-01-19 10:43:34 +0530

    updated bcrypt

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use('/api/cart',cartRouter)
 app.use('/api/order',orderRouter)
 
 app.get('/', (req, res) => {
-    res.send('Hello')
+    res.send('World')
 }) 
 
 cron.schedule('* * * * * *',()=>{
